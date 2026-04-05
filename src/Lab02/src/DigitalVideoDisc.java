0e9faa3032378b9f7bb8860463c3f0086a97dda3
Bao Pham <baophamnb06@gmail.com>
2026-04-05 08:53:56 +0700

    done section 7, 8, 9, 10

---
+++ src/Lab02/src/DigitalVideoDisc.java
@@ -0,0 +1,77 @@
+package Lab02.src;
+
+public class DigitalVideoDisc {
+    private int id;
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private float mass;
+
+    public DigitalVideoDisc(int id, String title, String category, String director, int length, float cost, float mass) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        this.mass = mass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public float getMass() {
+        return mass;
+    }
+
+    public void setMass(float mass) {
+        this.mass = mass;
+    }
+}
