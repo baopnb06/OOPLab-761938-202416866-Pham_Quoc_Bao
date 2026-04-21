cb49fac2fdbfbb6b6e8adf4d9e7291784a953fc3
Bao Pham <baophamnb06@gmail.com>
2026-04-21 22:32:59 +0700

    update_storeimpl

---
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -0,0 +1,44 @@
+package hust.soict.hedspi.aims.store;
+
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class Store {
+    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[100];
+    private int qtyInStore = 0;
+
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (qtyInStore < itemsInStore.length) {
+            itemsInStore[qtyInStore] = dvd;
+            qtyInStore++;
+            System.out.println("DVD \"" + dvd.getTitle() + "\" has been added to the store.");
+        } else {
+            System.out.println("The store is full! Cannot add more DVDs.");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc dvd) {
+        boolean found = false;
+        for (int i = 0; i < qtyInStore; i++) {
+            if (itemsInStore[i] == dvd) {
+                for (int j = i; j < qtyInStore - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+                itemsInStore[qtyInStore - 1] = null;
+                qtyInStore--;
+                found = true;
+                System.out.println("DVD \"" + dvd.getTitle() + "\" has been removed from the store.");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("DVD \"" + dvd.getTitle() + "\" not found in the store.");
+        }
+    }
+    public void printStore() {
+        System.out.println("\n**********STORE INVENTORY**********");
+        for (int i = 0; i < qtyInStore; i++) {
+            System.out.println((i + 1) + ". " + itemsInStore[i].toString());
+        }
+        System.out.println("***********************************\n");
+    }
+}
---
+++ AimsProject/src/hust/soict/hedspi/aims/store/StoreTest.java
@@ -0,0 +1,25 @@
+package hust.soict.hedspi.aims.store;
+
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store aimsStore = new Store();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+
+        aimsStore.addDVD(dvd1);
+        aimsStore.addDVD(dvd2);
+        aimsStore.addDVD(dvd3);
+
+        aimsStore.printStore();
+
+        aimsStore.removeDVD(dvd2);
+        aimsStore.printStore();
+
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Avatar", "Sci-fi", 25.0f);
+        aimsStore.removeDVD(dvd4);
+    }
+}
