e1e3de60364ceec24eb66b596f6bbfd878023a7c
Bao Pham <baophamnb06@gmail.com>
2026-06-09 22:53:03 +0700

    done section 12

--- Lab05/AimsProject/src/hust/soict/hedspi/aims/media/Media.java
+++ Lab05/AimsProject/src/hust/soict/hedspi/aims/media/Media.java
@@ -1,10 +1,9 @@
 package hust.soict.hedspi.aims.media;
 
-import org.w3c.dom.html.HTMLImageElement;
-
 import java.util.Comparator;
+import java.util.Objects;
 
-public abstract class Media {
+public abstract class Media implements Comparable<Media> {
     private int id;
     private String title;
     private String category;
@@ -30,18 +29,42 @@ public abstract class Media {
     public String toString() {
         return getTitle() + " - " + getCategory();
     }
+
     @Override
     public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
         if (!(obj instanceof Media)) {
             return false;
         }
         Media other = (Media) obj;
-
-        if (this.title == null || other.getTitle() == null) {
-            return false;
-        }
-        return this.title.equalsIgnoreCase(other.getTitle());
+        return Objects.equals(this.title, other.getTitle())
+                && Float.compare(this.cost, other.getCost()) == 0;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, cost);
+    }
+
+    @Override
+    public int compareTo(Media other) {
+        if (other == null) {
+            throw new NullPointerException("Cannot compare Media with null");
+        }
+
+        String thisTitle = this.title == null ? "" : this.title;
+        String otherTitle = other.getTitle() == null ? "" : other.getTitle();
+
+        int titleDiff = thisTitle.compareToIgnoreCase(otherTitle);
+        if (titleDiff != 0) {
+            return titleDiff;
+        }
+
+        return Float.compare(this.cost, other.getCost());
+    }
+
     public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
     public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
 
